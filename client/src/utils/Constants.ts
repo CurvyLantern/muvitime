3844350803a1a76f3cb888436306119ee6e91e38
naseem(T_T) <ashfaqnaseem1@gmail.com>
2023-08-08 01:46:05 +0600

    changed backend url to render

--- client/src/utils/Constants.ts
+++ client/src/utils/Constants.ts
@@ -5,6 +5,6 @@ export const roomIdSyntax = "aaa-aaaa-aaa";
 export const socketPath =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://muvitime-production.up.railway.app";
+    : "https://muvitime-backend.onrender.com";
 
 export const ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
